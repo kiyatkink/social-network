2c0c82877cd1c2d445a18b3c177befafd2717c7e
Kirill K <kkiyatkin@hwdtech.ru>
2023-07-05 12:09:12 +0600

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+
+  parser: '@typescript-eslint/parser',
+
+  overrides: [],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  rules: {
+    indent: ['error', 2, { ignoredNodes: ['JSXElement *', 'JSXElement'] }],
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/function-component-definition': [2, { namedComponents: ['function-declaration', 'function-expression', 'arrow-function'] }],
+    'no-shadow': 'off',
+    'react/button-has-type': 'off',
+  },
+};
